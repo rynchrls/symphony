6de67fece7ec02dfb4fdd8438c95acf48281e205
Ryan Charles <rynchrls18@gmail.com>
2025-01-25 08:44:20 +0800

    deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import store from "./store/store.js";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://symphony-82vd.onrender.com";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
